71bc728a8784b0703fa1314e7d3f4ccfd7553afe
kartiks-git <166221473+kartiks-git@users.noreply.github.com>
2024-05-17 00:07:46 +0530

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,15 @@
+// Write a function that takes in the radius as input and returns the circumference of a circle.
+import java.util.*;
+class Question4 {
+    public static double Circum(double r) {
+        double cir = 2*3.14*r;
+        return cir;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r = sc.nextDouble();
+
+        double cir = Circum(r);
+        System.out.println("The circumference of the circle is : "+cir);
+    }
+}
