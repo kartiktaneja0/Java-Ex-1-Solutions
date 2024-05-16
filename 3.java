a6e48662f0a103c620b7e00106427be0be44da0f
kartiks-git <166221473+kartiks-git@users.noreply.github.com>
2024-05-16 21:09:55 +0530

    Create 3.java

---
+++ 3.java
@@ -0,0 +1,20 @@
+// Write a function which takes in 2 numbers and returns the greater of those two.
+import java.util.*;
+class Question3 {
+    public static int Greater(int a, int b) {
+        if(a>b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2 numbers below")
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("The Greater number is : "+Greater(a,b));
+    }
+}
