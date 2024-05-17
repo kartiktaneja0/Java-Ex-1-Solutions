0ce23c4bf935c49b8d08ff60215a4ffdaf29fbfb
kartiks-git <166221473+kartiks-git@users.noreply.github.com>
2024-05-17 15:04:25 +0530

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,10 @@
+// Write an infinite loop using do while condition.
+class Question6 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("Infinite loop");
+            i++;
+        } while(i>0);
+    }
+}
