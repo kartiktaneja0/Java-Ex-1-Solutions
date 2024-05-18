c9bce9641e1e94dc0bf2aa799f7b3c6da4151a01
kartiks-git <166221473+kartiks-git@users.noreply.github.com>
2024-05-18 14:27:56 +0530

    Create 9.java

---
+++ 9.java
@@ -0,0 +1,20 @@
+// Write a function that calculates the Greatest Common Divisor of 2 numbers.
+import java.util.*;
+class Question9 {
+    public static void HCF(int a, int b) {
+        int hcf = 0;
+        for(int i=1;i<=a && i<=b;i++) {
+            if(a%i==0 && b%i==0) {
+                hcf = i;
+            }
+        }
+        System.out.println("The HCF is : "+hcf);
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        HCF(a,b);
+    }
+}
